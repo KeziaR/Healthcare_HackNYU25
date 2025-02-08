7749059d627041dfdcceed90624d52d5c8662dc9
Knregis <Regiskezia4@gmail.com>
2025-02-08 16:28:02 -0500

    plain html to streamlit html

--- Streamlit_Webapp/Web_interface.py
+++ Streamlit_Webapp/Web_interface.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import requests as rq
+from google import genai
+import config
+
+
+
+st.title("TitleName")
+
+client = genai.Client(api_key=config.INFO)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="who is kylie jenner",
+)
+
+print(response.text)
 
 st.html(body)
