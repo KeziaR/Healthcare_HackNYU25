7c65fbec39d7536d2ced3bf639fe7f2626b9e9a0
Knregis <Regiskezia4@gmail.com>
2025-02-08 14:48:37 -0500

    added how to download libraries

--- Chatbot/API_Creation.py
+++ Chatbot/API_Creation.py
@@ -3,7 +3,7 @@ from google import genai
 import config
 
 
-client = genai.Client(api_key=INFO.config)
+client = genai.Client(api_key=config.INFO)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
