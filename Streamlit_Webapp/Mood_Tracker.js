a1347a439ff54b84c282ba0e52d97d3973b23028
Knregis <Regiskezia4@gmail.com>
2025-02-08 20:22:50 -0500

    starting anew

---
+++ Streamlit_Webapp/Mood_Tracker.js
@@ -0,0 +1,18 @@
+
+import html as pic
+
+
+st.title("Mood Tracker")
+
+function Click_Image(img_path, label="", width=100, height=50):
+    happy_button = st.button()
+    html_str = f"<button><img src='{image_path}' width='{width}' height='{height}'>{label}</button>"
+    components.html(html_str, height=height)
+Click_Image()
+
+function mood():
+    count = 0 
+
+    if 'count' not in st.session_state
+    if mood == 
+
--- Streamlit_Webapp/Web_interface.py
+++ Streamlit_Webapp/Web_interface.py
@@ -51,33 +51,3 @@ Contributors:
 components.html(web_app, height=800, scrolling=True)
 
 
-choice = st.radio('Background Type', ['Image', 'Gradient'])
-
-if choice == 'Image':
-    st.title('Image Background')
-    st.markdown('Boop! :ghost:')
-
-    image = './app/static/house-panther-nose.png'
-
-    css = f'''
-    <style>
-        .stApp {{
-            background-image: url({image});
-            background-size: cover;
-
-        }}
-        .stApp > header {{
-            background-color: transparent;
-        }}
-    </style>
-    '''
-    st.markdown(css, unsafe_allow_html=True)
-
-else:
-    st.title('Gradient Background')
-    st.write('Look at the pretty shifting background')
-
-    with open('./files/wave.css') as f:
-        css = f.read()
-
-    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
