0970d529e7826a43817e0eba4f97f53ea87162dd
Sommer Batchelor <153052068+Runwithwings@users.noreply.github.com>
2025-02-08 19:11:19 -0500

    added css to web interface

--- Streamlit_Webapp/Web_interface.py
+++ Streamlit_Webapp/Web_interface.py
@@ -2,6 +2,30 @@ import streamlit as st
 import requests as rq
 from google import genai
 import streamlit.components.v1 as components
+<<<<<<< HEAD
+=======
+
+
+#import config 
+
+
+
+# st.title("TitleName")
+
+# client = genai.Client(api_key=config.INFO)
+
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash",
+#     contents="who is kylie jenner",
+# )
+
+# print(response.text)
+
+# st.html(body)
+
+
+
+>>>>>>> 7cd17d1 (added css to web interface)
 
 
     
@@ -11,8 +35,8 @@ web_app = """
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 //May change later - depends on what we use
 
-<title>This is the title</title>
-//Title will be added later
+<title>How Do I Feel Today? App</title>
+//Working title
 
 <link rel="stylesheet" href="style.css" type="text/css"/>
 <nav>
@@ -23,20 +47,25 @@ web_app = """
 </nav>
 //Add navigation bar later
 
+<h1>How Do I Feel Today?</h1>
+
 </head>
 <body>
-<aside>
+<aside class="chatbox">
 
 </aside>
 //Chatbox goes here
 
 </body>
-//Should include mood tracker and other features
+<section class="moodtracker"></section>
+
 
 <footer>
 
 </footer>
-//Miscillaneous information
+Contributors:
+<ul>
+</ul>
 
 </html>
 """
@@ -44,3 +73,7 @@ web_app = """
 components.html(web_app, height=800, scrolling=True)
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7cd17d1 (added css to web interface)
