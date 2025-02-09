0308fc3cc15c1bcf5ba5839600e4f43472bb70bf
Knregis <Regiskezia4@gmail.com>
2025-02-09 05:21:58 -0500

    trying to get the mood tracker working

--- Streamlit_Webapp/Mood_Tracker.js
+++ Streamlit_Webapp/Mood_Tracker.js
@@ -1,18 +1,16 @@
 
-import html as pic
 
 
-st.title("Mood Tracker")
+//---------------------------------------------------------------------------
+
+
 
 function Click_Image(img_path, label="", width=100, height=50):
-    happy_button = st.button()
-    html_str = f"<button><img src='{image_path}' width='{width}' height='{height}'>{label}</button>"
-    components.html(html_str, height=height)
-Click_Image()
 
-function mood():
-    count = 0 
+    pass
 
-    if 'count' not in st.session_state
-    if mood == 
+
+function Mood():
+    pass
+    
 
